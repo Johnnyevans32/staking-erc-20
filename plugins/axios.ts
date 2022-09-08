3c6d0d1f7cc3bba7fc9b7f6524ae2d356d44b5e6
johnny evans <evanseburu32@gmail.com>
2022-09-08 15:51:57 +0100

    init

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -5,18 +5,5 @@ export default function ({ $axios, redirect, store, route }) {
 
   $axios.onError((error: { response: { status: any } }) => {
     const code = parseInt(error.response && error.response.status);
-
-    if (code === 401) {
-      const { path } = route;
-      if (path === '/auth') {
-        return;
-      }
-
-      store.commit('user/_SET_USER_TOKEN', null);
-      store.commit('user/_SET_USER_DATA', null);
-      $axios.setToken(false);
-
-      return redirect('/auth');
-    }
   });
 }
